57ce3cad828f797f7a3b1d5cf9856e19b2a340ee
Toya-Onodera <gtrzexel@gmail.com>
2019-12-03 00:57:08 +0900

    QR コード読み取り後の簡易動作を実装

--- app/src/main/java/com/sd_lab/ipuprojectexercise2019/QRReaderActivity.kt
+++ app/src/main/java/com/sd_lab/ipuprojectexercise2019/QRReaderActivity.kt
@@ -33,22 +33,17 @@ class QRReaderActivity : AppCompatActivity() {
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
 
         if (result != null) {
-            if (result.contents == null) {
-                Toast.makeText(this, result.contents, Toast.LENGTH_LONG).show()
-            } else {
-                // If QRCode contains data.
-                try {
-                    // Converting the data to json format
-                    val obj = JSONObject(result.contents)
+            // TODO: Kotlin には三項演算子がない
+            val resultStr = if (result.contents == "成功") true else false
 
-                } catch (e: JSONException) {
-                    e.printStackTrace()
-
-
-                }
-
+            // 利用者が購入したパン (QRコード) の場合
+            if (resultStr) {
+                // TODO: リザルト画面のアクティビティを表示する
+                Toast.makeText(this, "成功", Toast.LENGTH_LONG).show()
             }
-        } else {
+        }
+
+        else {
             super.onActivityResult(requestCode, resultCode, data)
         }
     }
