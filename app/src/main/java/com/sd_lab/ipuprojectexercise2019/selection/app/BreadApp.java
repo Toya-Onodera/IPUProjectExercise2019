c126f8dc16e270e0670d24028ac6c9b030b4e120
猪股一歩希 <ibuki.i1130bobby@gmail.com>
2019-12-04 14:04:54 +0900

    BreadAppクラスを修正

--- app/src/main/java/com/sd_lab/ipuprojectexercise2019/selection/app/BreadApp.java
+++
@@ -1,4 +0,0 @@
-package com.sd_lab.ipuprojectexercise2019.selection.app;
-
-public class BreadApp {
-}
---
+++ app/src/main/java/com/sd_lab/ipuprojectexercise2019/selection/app/BreadApp.kt
@@ -0,0 +1,4 @@
+package com.sd_lab.ipuprojectexercise2019.selection.app
+
+class BreadApp {
+}
