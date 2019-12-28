360d52b9a0ced6e6b477532ea57e1b5761a5fc23
Toya-Onodera <gtrzexel@gmail.com>
2019-12-28 14:16:11 +0900

    メニュー選択機能を実装（アクティビティ間のやり取りはまだ）

--- app/src/main/java/com/sd_lab/ipuprojectexercise2019/SelectionMainActivity.kt
+++ app/src/main/java/com/sd_lab/ipuprojectexercise2019/SelectionMainActivity.kt
@@ -1,16 +1,46 @@
 package com.sd_lab.ipuprojectexercise2019
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ListView
-import com.sd_lab.ipuprojectexercise2019.R
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_selection_main.*
+
 
 class SelectionMainActivity : AppCompatActivity() {
+    private var breadMenuArray: Array<String>? = null
+    private var boughtMenuArray: MutableList<String> = mutableListOf()
+    private var boughtMenuText: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selection_main)
 
-//        val listView = findViewById<ListView>(R.id.lvAll)
+        breadMenuArray = resources.getStringArray(R.array.bread_menu)
+
+        // メニューをクリックしたときの動作を書く
+        breadList.setOnItemClickListener{ parent, v, position, id ->
+            boughtMenuArray.add(breadMenuArray!![position])
+            boughtMenuText = ""
+
+            boughtMenuArray.forEachIndexed { i, e ->
+                boughtMenuText += if (i != 0) ", ${e}" else "${e}"
+            }
+
+            buiedText.text = boughtMenuText
+        }
+
+        // メニュー確定
+        bt_confirmOrder.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("注文を確定してもよろしいでしょうか？")
+                .setMessage(boughtMenuText)
+                .setPositiveButton("OK", { d, w ->
+                    // TODO: 次のアクティビティに飛ばす
+                })
+                .setNegativeButton("No", { d, w ->
+                    // 何もしない
+                })
+                .show()
+        }
     }
 }
